7973bff53d68d922a1ff2bcc5e87bdf95d586463
manpercar1 <47862315+manpercar1@users.noreply.github.com>
2022-05-15 20:56:23 +0200

    Revert "Inicialización jugadores"

--- TeamKraken/apps/jugadores/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from apps.jugadores.models import *
-
-# Register your models here.
-
-admin.site.register(Posicion)
-admin.site.register(Jugador)
-admin.site.register(Cooper)
